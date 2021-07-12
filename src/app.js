5ea998c1c71314b68ba38aa1fd02333a13a6fb3a
Su401 <81376141+Su401@users.noreply.github.com>
2021-07-12 20:34:10 +0100

    added current time
    removed console.log

--- src/app.js
+++ src/app.js
@@ -1,19 +1,45 @@
- function cityWeather(response){
-   console.log(response.data);
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = 
+  [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday"
+  ]
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  if (hours <10 ) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes(); 
+  if (minutes <10 ) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`
+}
+
+
+function cityWeather(response){
    let temperatureElement = document.querySelector("#temp");
    let cityElement = document.querySelector("#current-location");
    let weatherDescriptionElement = document.querySelector("#description");
    let humidityElement = document.querySelector("#humidity");
    let windElement = document.querySelector("#wind");
+   let currentTimeElement = document.querySelector("#current-time");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     weatherDescriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    currentTimeElement.innerHTML = formatDate(response.data.dt * 1000);
  }
   
  
  let apiKey = "6bf9818d9ac6ad65c210c2c0a7205a25";
  let units = "metric";
- let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=porto&appid=${apiKey}&units=${units}`;
+ let city = "porto"
+ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
  axios.get(apiUrl).then(cityWeather);
