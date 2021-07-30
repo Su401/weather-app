889cc02a2858003d78bb7bf7af6b4be6774f736d
Su401 <81376141+Su401@users.noreply.github.com>
2021-07-30 08:25:23 +0100

    added the C/F conversion

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours <10 ) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes(); 
+  if (minutes <10 ) {
+    minutes = `0${minutes}`;
+  }
   let days = 
   [
     "Sunday",
@@ -10,17 +18,8 @@ function formatDate(timestamp) {
     "Friday"
   ]
   let day = days[date.getDay()];
-  let hours = date.getHours();
-  if (hours <10 ) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes(); 
-  if (minutes <10 ) {
-    minutes = `0${minutes}`;
-  }
   return `${day} ${hours}:${minutes}`
 }
-
 
 function cityWeather(response){
    let temperatureElement = document.querySelector("#temp");
@@ -31,18 +30,20 @@ function cityWeather(response){
    let currentTimeElement = document.querySelector("#current-time");
   let iconElement = document.querySelector("#icon");
 
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
-    cityElement.innerHTML = response.data.name;
-    weatherDescriptionElement.innerHTML = response.data.weather[0].description;
-    humidityElement.innerHTML = response.data.main.humidity;
-    windElement.innerHTML = Math.round(response.data.wind.speed);
-    currentTimeElement.innerHTML = formatDate(response.data.dt * 1000);
-    iconElement.setAttribute(
-      "src", 
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` ); 
-    iconElement.setAttribute(
-      "alt",
-      response.data.weather[0].description);
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  weatherDescriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  currentTimeElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src", 
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` ); 
+  iconElement.setAttribute(
+    "alt",
+    response.data.weather[0].description);
   }
   
 function search(city) {
@@ -57,9 +58,35 @@ function search(city) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
-  console.log(cityInputElement.value);
 }
  
 
- let form = document.querySelector("#search-form");
- form.addEventListener("submit", handleSubmit);
+function convertFsmth(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temp");
+  //convertCelsius.classList.remove("active");
+  //convertFsmth.classList.add("active");
+  let fsmthTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fsmthTemperature);
+}
+
+
+function convertCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+ // convertCelsius.classList.add("active");
+ // convertFsmth.classList.remove("active");
+}
+
+
+let celsiusTemperature = null;
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fsmth");
+fahrenheitLink.addEventListener("click", convertFsmth);
+
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", convertCelsius);
